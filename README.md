26c567bb2639e854d3b53f4100bb845416507e4d
Axelena <axelena@yandex.ru>
2025-11-24 12:23:05 +1100

    Toy robot simulator

---
+++ README.md
@@ -0,0 +1,31 @@
+## Toy Robot
+
+A simulator of a toy robot, moving on a tabletop
+
+## Introduction
+
+This console application simulates a toy robot, moving on a tabletop (5x5). The robot can be placed, move, turn left/right and report it's location. It cannot fall off the table.
+(This reminded me my school days when I started programming and we had so much fun working on similar tasks :) )
+
+## How to run 
+
+    git clone git://github.com/axelena/toyrobot.git
+    cd toyrobot
+    gem install bundler
+    bundle install
+
+ Then simple execution:
+
+    ruby toyrobot.rb
+
+Commands can be fed directly into STDIN 
+ (a file with test data can be provided bu request)
+
+
+## Testing
+
+RSpec testing
+
+
+
+
---
+++ lib/robot.rb
@@ -0,0 +1,89 @@
+require_relative 'table'
+
+class Robot
+
+  attr_reader :x_position, :y_position, :vector
+
+  def initialize(table)
+    @table = table
+  end
+
+  def place(position_x, position_y, vector) 	
+    if @table.valid_vector?(vector)
+      @x_position = position_x
+      @y_position = position_y
+      @vector = vector
+    else
+      print "Wrong coordinates"
+    end
+  end
+
+  def move
+    if placed?
+      case @vector
+        when "NORTH" 
+          if @table.inside_table?((@y_position+1), @x_position)
+            @y_position += 1 
+          end
+        when "EAST" 
+          if @table.inside_table?(@y_position, @x_position+1) 
+            @x_position += 1
+          end
+        when "SOUTH" 
+          if @table.inside_table?(@y_position-1, @x_position) 
+            @y_position -= 1
+          end
+        when "WEST" 
+          if @table.inside_table?(@y_position, @x_position-1)  
+            @x_position -= 1
+          end
+      end
+
+      move_to(@x_position,@y_position)
+      
+    else
+      puts "The robot hasn't been placed yet"
+    end
+
+  end
+
+  def left
+    new_vector = @vector == vectors.first ? vectors.last : vectors[vector_index - 1]
+    change_vector(new_vector)    
+  end
+
+  def right
+    new_vector = @vector == vectors.last ? vectors.first : vectors[vector_index + 1]
+    change_vector(new_vector) 
+  end
+
+  def location 
+    "#{@x_position},#{@y_position},#{@vector}" if placed?
+  end
+
+  def placed?
+    !(@vector.nil?)
+  end
+
+
+  private
+
+  def move_to(position_x, position_y) 
+    unless @table.inside_table?(position_x, position_y)
+      puts "Can't be moved there"
+    end
+  end
+
+  def change_vector(vector) 
+    @vector = vector
+    puts "The robot has been rotated to #{vector}"
+  end
+
+  def vectors
+    Table::VALID_VECTORS
+  end
+
+  def vector_index
+    vectors.index(@vector)
+  end
+end
